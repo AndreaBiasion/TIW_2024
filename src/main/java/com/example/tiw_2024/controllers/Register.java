8c9afb83c6a38c7ba69b8f349340fc4bbdeb1c83
Omar Chaabani <omarchaabani22@gmail.com>
2024-05-02 13:31:55 +0200

    feat: moved folders + add Register.java + javadoc

--- src/main/java/com/example/tiw_2024/controllers/Register.java
+++ src/main/java/com/example/tiw_2024/controllers/Register.java
@@ -2,26 +2,88 @@ package com.example.tiw_2024.controllers;
 
 import com.example.tiw_2024.utils.ConnectionManager;
 import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.SQLException;
 
+/**
+ * This servlet class handles the registration process.
+ * It provides methods for handling HTTP GET and POST requests related to registration.
+ */
 public class Register extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
-    private Connection connection;
+    private Connection connection = null;
     private TemplateEngine templateEngine;
 
+    /**
+     * Default constructor.
+     */
     public Register(){
         super();
     }
 
+    /**
+     * Initializes the servlet by establishing a database connection and configuring the template engine.
+     * @throws ServletException if an error occurs during servlet initialization.
+     */
     @Override
     public void init() throws ServletException {
         connection = ConnectionManager.getConnection(getServletContext());
         ServletContext servletContext = getServletContext();
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        this.templateEngine = new TemplateEngine();
+        this.templateEngine.setTemplateResolver(templateResolver);
+        templateResolver.setSuffix(".html");
+    }
 
+    /**
+     * Handles HTTP GET requests for registration page.
+     * @param req  the HttpServletRequest object containing the request parameters.
+     * @param resp the HttpServletResponse object for sending the response.
+     * @throws ServletException if an error occurs while processing the request.
+     * @throws IOException      if an I/O error occurs while handling the request.
+     */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String path = "/WEB-INF/register.html";
+        resp.setContentType("text/html");
+        ServletContext servletContext = getServletContext();
+        final WebContext webContext = new WebContext(req, resp, servletContext, req.getLocale());
+        templateEngine.process(path, webContext, resp.getWriter());
+    }
+
+    /**
+     * Handles HTTP POST requests for registration.
+     * @param req  the HttpServletRequest object containing the request parameters.
+     * @param resp the HttpServletResponse object for sending the response.
+     * @throws ServletException if an error occurs while processing the request.
+     * @throws IOException      if an I/O error occurs while handling the request.
+     */
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Implementation for handling registration POST requests goes here
+    }
+
+    /**
+     * Cleans up resources used by the servlet.
+     */
+    @Override
+    public void destroy() {
+        try {
+            ConnectionManager.closeConnection(connection);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/example/tiw_2024/dao/UserDAO.java
+++ src/main/java/com/example/tiw_2024/dao/UserDAO.java
@@ -7,31 +7,41 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * This class handles data access operations related to users.
+ */
 public class UserDAO {
 
-    Connection connection;
+    private Connection connection;
 
-
+    /**
+     * Constructs a new UserDAO with the given database connection.
+     * @param connection the database connection to be used by the DAO.
+     */
     public UserDAO(Connection connection) {
         this.connection = connection;
     }
 
+    /**
+     * Checks user credentials against the database.
+     * @param email the email of the user.
+     * @param password the password of the user.
+     * @return a User object if the credentials are valid, null otherwise.
+     * @throws SQLException if an SQL exception occurs while accessing the database.
+     */
     public User checkCredentials(String email, String password) throws SQLException {
         String query = "SELECT * FROM utente WHERE email = ? AND password = ?";
         ResultSet resultSet = null;
-
         PreparedStatement preparedStatement = null;
 
         try {
             preparedStatement = connection.prepareStatement(query);
-
             preparedStatement.setString(1, email);
             preparedStatement.setString(2, password);
             resultSet = preparedStatement.executeQuery();
 
-            // in case of no results
             if (!resultSet.next()) {
-                return null;
+                return null; // No user found with the provided credentials
             } else {
                 resultSet.next();
                 User user = new User();
@@ -40,24 +50,27 @@ public class UserDAO {
                 user.setSurname(resultSet.getString("surname"));
                 user.setEmail(resultSet.getString("email"));
                 user.setPassword(resultSet.getString("password"));
-
-                return user;
+                return user; // Return the found user
             }
         } finally {
-
             if (resultSet != null) {
                 resultSet.close();
             }
-
             if (preparedStatement != null) {
                 preparedStatement.close();
             }
         }
-
     }
 
+    /**
+     * Adds a new user to the database.
+     * @param name the name of the user.
+     * @param surname the surname of the user.
+     * @param email the email of the user.
+     * @param password the password of the user.
+     * @throws SQLException if an SQL exception occurs while accessing the database.
+     */
     public void addUser(String name, String surname, String email, String password) throws SQLException {
-
         String query = "INSERT INTO utente (nome, cognome, email, password) VALUES (?, ?, ?, ?)";
         PreparedStatement preparedStatement = null;
 
@@ -67,14 +80,11 @@ public class UserDAO {
             preparedStatement.setString(2, surname);
             preparedStatement.setString(3, email);
             preparedStatement.setString(4, password);
-            preparedStatement.executeUpdate();
+            preparedStatement.executeUpdate(); // Execute the insert query
         } finally {
             if (preparedStatement != null) {
                 preparedStatement.close();
             }
         }
-
     }
-
-
 }
